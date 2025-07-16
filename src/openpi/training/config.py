b2f7f2a2ae79393667526b263b9dfcd5719ce3b1
Jenny Pan <jrpan@iris-hgx-2.stanford.edu>
2025-07-16 16:53:20 -0700

    More edits

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -336,14 +336,6 @@ class LeRobotRoboMemoryDataConfig(DataConfigFactory):
 
     @override
     def create(self, assets_dirs: pathlib.Path, model_config: _model.BaseModelConfig) -> DataConfig:
-        # The repack transform is *only* applied to the data coming from the dataset,
-        # and *not* during inference. We can use it to make inputs from the dataset look
-        # as close as possible to those coming from the inference environment (e.g. match the keys).
-        # Below, we match the keys in the dataset (which we defined in the data conversion script) to
-        # the keys we use in our inference pipeline (defined in the inference script for libero).
-        # For your own dataset, first figure out what keys your environment passes to the policy server
-        # and then modify the mappings below so your dataset's keys get matched to those target keys.
-        # The repack transform simply remaps key names here.
         repack_transform = _transforms.Group(
             inputs=[
                 _transforms.RepackTransform(
@@ -359,39 +351,20 @@ class LeRobotRoboMemoryDataConfig(DataConfigFactory):
             ]
         )
 
-        # The data transforms are applied to the data coming from the dataset *and* during inference.
-        # Below, we define the transforms for data going into the model (``inputs``) and the transforms
-        # for data coming out of the model (``outputs``) (the latter is only used during inference).
-        # We defined these transforms in `libero_policy.py`. You can check the detailed comments there for
-        # how to modify the transforms to match your dataset. Once you created your own transforms, you can
-        # replace the transforms below with your own.
         data_transforms = _transforms.Group(
             inputs=[robomemory_policy.RoboMemoryInputs(action_dim=model_config.action_dim, model_type=model_config.model_type)],
             outputs=[robomemory_policy.RoboMemoryOutputs()],
         )
 
-        # One additional data transform: pi0 models are trained on delta actions (relative to the first
-        # state in each action chunk). IF your data has ``absolute`` actions (e.g. target joint angles)
-        # you can uncomment the following line to convert the actions to delta actions. The only exception
-        # is for the gripper actions which are always absolute.
-        # In the example below, we would apply the delta conversion to the first 6 actions (joints) and
-        # leave the 7th action (gripper) unchanged, i.e. absolute.
-        # In Libero, the raw actions in the dataset are already delta actions, so we *do not* need to
-        # apply a separate delta conversion (that's why it's commented out). Choose whether to apply this
-        # transform based on whether your dataset uses ``absolute`` or ``delta`` actions out of the box.
+        # We assume joint *velocity* actions, so we should *not* apply an additional delta transform. Thus the below lines are commented out.
+        # delta_action_mask = _transforms.make_bool_mask(7, -1)
+        # data_transforms = data_transforms.push(
+        #     inputs=[_transforms.DeltaActions(delta_action_mask)],
+            # outputs=[_transforms.AbsoluteActions(delta_action_mask)],
+        # )
 
-        # TODO: check this
-        delta_action_mask = _transforms.make_bool_mask(7, -1)
-        data_transforms = data_transforms.push(
-            inputs=[_transforms.DeltaActions(delta_action_mask)],
-            outputs=[_transforms.AbsoluteActions(delta_action_mask)],
-        )
-
-        # Model transforms include things like tokenizing the prompt and action targets
-        # You do not need to change anything here for your own dataset.
         model_transforms = ModelTransformFactory()(model_config)
 
-        # We return all data transforms for training and inference. No need to change anything here.
         return dataclasses.replace(
             self.create_base_config(assets_dirs),
             # repack_transforms=repack_transform,
@@ -502,7 +475,7 @@ class TrainConfig:
     # How often (in steps) to save checkpoints.
     save_interval: int = 1000
     # If set, any existing checkpoints matching step % keep_period == 0 will not be deleted.
-    keep_period: int | None = 5000
+    keep_period: int | None = 2000
 
     # If true, will overwrite the checkpoint directory if it already exists.
     overwrite: bool = False
@@ -771,9 +744,9 @@ _CONFIGS = [
             action_dim=8, action_horizon=10, max_token_len=180, paligemma_variant="gemma_2b_lora"
         ),
         data=LeRobotRoboMemoryDataConfig(
-            repo_id="jennypan00/pi0_fast_ft_droid_lerobot",
+            repo_id="jennypan00/pi0_fast_ft_droid_lerobot_train",
             base_config=DataConfig(prompt_from_task=True),
-            assets=AssetsConfig( # TODO: check this
+            assets=AssetsConfig(
                 assets_dir="/iris/u/jrpan/openpi/assets",
                 asset_id="pi0_fast_robomemory_low_mem_finetune/jennypan00/pi0_fast_ft_droid_lerobot",
             ),
@@ -791,15 +764,15 @@ _CONFIGS = [
             action_dim=8, action_horizon=10, max_token_len=180
         ),
         data=LeRobotRoboMemoryDataConfig(
-            repo_id="jennypan00/pi0_fast_ft_droid_lerobot",
+            repo_id="jennypan00/pi0_fast_ft_droid_lerobot_train",
             base_config=DataConfig(prompt_from_task=True),
-            assets=AssetsConfig( # TODO: check this
+            assets=AssetsConfig( 
                 assets_dir= "/iris/u/jrpan/openpi/assets", # TODO: check "gs://openpi-assets/checkpoints/pi0_fast_base/assets",
-                asset_id="pi0_fast_robomemory_finetune/jennypan00/pi0_fast_ft_droid_lerobot",
+                asset_id="pi0_fast_robomemory_low_mem_finetune/jennypan00/pi0_fast_ft_droid_lerobot",
             ),
         ),
         weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi0_fast_droid/params"),
-        num_train_steps=30_000,
+        num_train_steps=10_000,
     ),
     #
     # ALOHA Sim configs. This config is used to demonstrate how to train on a simple simulated environment.
